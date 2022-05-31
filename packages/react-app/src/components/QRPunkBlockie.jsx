4fb94f0910fe0eaa7c0f6c54791ace1607363ec5
tamas.molnar <tamas.molnar@liferay.com>
2022-05-31 15:31:30 +0200

    Create CryptoPunks link on the image

--- packages/react-app/src/components/QRPunkBlockie.jsx
+++ packages/react-app/src/components/QRPunkBlockie.jsx
@@ -61,7 +61,9 @@ export default function QRPunkBlockie(props) {
 
       <div style={{position:"absolute",left:hardcodedSizeForNow/2-53,top:hardcodedSizeForNow/2-65}}>
         <div style={{position:"relative",width:punkSize, height:punkSize-1, overflow: "hidden"}}>
-          <img src="/punks.png" style={{position:"absolute",left:-punkSize*x,top:(-punkSize*y)-1,width:punkSize*100, height:punkSize*100,imageRendering:"pixelated"}} />
+          <a href={"https://cryptopunks.app/cryptopunks/details/" + (y * 100 + x)}>
+            <img src="/punks.png" style={{position:"absolute",left:-punkSize*x,top:(-punkSize*y)-1,width:punkSize*100, height:punkSize*100,imageRendering:"pixelated"}} />
+          </a>
         </div>
       </div>
 
